2ffb1ed8b382484165634ad92f182dd5bee79301
lysabacate <133353329+lysabacate@users.noreply.github.com>
2025-06-18 00:42:20 +0000

    estilizando telas + criando novas

--- app.py
+++ app.py
@@ -52,6 +52,14 @@ def registrar():
 def mensagem():
     return render_template('mensagem.html')
 
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
+
+@app.route('/editar_dados')
+def editar_dados():
+    return render_template('editar-dados.html')
+
 register_error_handlers(app)
 
 if __name__ == '__main__':
